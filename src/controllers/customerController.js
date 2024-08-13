75e48634836fb3232b7b08b0bbb6d9a36083e6e4
CSSamrah <samra.fatima.790@gmail.com>
2024-08-13 17:49:31 +0500

    changes for deployment

---
+++ src/controllers/customerController.js
@@ -0,0 +1,93 @@
+const pool=require('../db')
+const bcrypt = require('bcryptjs');
+
+//  get customer information
+const getCustomerInfo= async (req, res) => {
+    try {
+        const selectQuery = 'SELECT * FROM customer';
+        const result = await pool.query(selectQuery);
+        res.json(result.rows)
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+};
+
+//  get registered customer information
+const getRegisteredCustomer= async (req, res) => {
+    try {
+        const selectQuery = 'SELECT * FROM registration';
+        const result = await pool.query(selectQuery);
+        res.json(result.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+};
+
+ const getRegCustomerById=async (req, res) => {
+    const { user_id } = req.params;
+    try {
+        const selectQuery = 'SELECT * FROM registration WHERE user_id = $1';
+        const result = await pool.query(selectQuery, [user_id]);
+        if (result.rows.length > 0) {
+            res.json(result.rows[0]);
+        } else {
+            res.status(404).send('registered customer detail not found');
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+};
+
+// insert customer information(sign up)
+const signUp= async (req, res) => {
+    try {
+        const { username, password, email, phoneno } = req.body;
+
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        const addcustomer = await pool.query('INSERT INTO registration(username,password,email,phoneno)values($1, $2, $3, $4)', [username, hashedPassword, email, phoneno])
+        res.status(201).send(' registered customer added successfully');
+    } catch (error) {
+        console.error(error.message)
+    }
+};
+
+// sign in
+
+ const signIn=async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // Fetch customer by email
+        const customerQuery = await pool.query('SELECT password FROM registration WHERE email = $1', [email]);
+        console.log(customerQuery.rows); // Log query result
+
+        if (customerQuery.rowCount > 0) {
+            const hashedPassword = customerQuery.rows[0].password;
+
+            bcrypt.compare(password, hashedPassword, (err, result) => {
+                if (err) {
+                    res.status(500).json({ error: 'Internal server error' });
+                } else if (result) {
+                    res.status(200).json({ message: 'Login successful' });
+                } else {
+                    res.status(401).json({ error: 'Invalid username or password' });
+                }
+            });
+        } else {
+            res.status(404).json({ error: 'registered user not found' });
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error');
+    }
+};
+
+
+module.exports={
+    getCustomerInfo,getRegisteredCustomer,getRegCustomerById,signIn,signUp
+}
---
+++ src/controllers/productsController.js
@@ -0,0 +1,120 @@
+const pool=require('../db')
+
+
+// add product in product table
+const addingProduct=async(req,res)=>{
+    try {
+        const { product_id, product_name, description, price, quantity, category_id, rating } = req.body;
+        const picture = req.file.buffer;
+
+        const addProduct = await pool.query('INSERT INTO product(product_id,product_name,description,price,picture,quantity,category_id,rating)values($1, $2, $3, $4, $5, $6, $7, $8)', [product_id, product_name, description, price, picture, quantity, category_id, rating])
+        res.status(201).send('Product added successfully');
+    } catch (error) {
+        console.error(error.message)
+    }
+}
+// get product details
+const getProductDetails=async(req,res)=>{
+    const { product_id } = req.params;
+  
+    try {
+      const selectQuery = 'SELECT * FROM product WHERE product_id = $1';
+      const result = await pool.query(selectQuery, [product_id]);
+  
+      if (result.rows.length > 0) {
+        const product = result.rows[0];
+        // Assuming 'image_data' is the column containing bytea data
+        const imageData = product.picture.toString('base64');
+        const productWithBase64Image = {
+          ...product,
+          image_data: imageData,
+        };
+        res.json(productWithBase64Image);
+      } else {
+        res.status(404).send('Product not found');
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+}
+
+//delete product
+const deleteProduct= async (req, res) => {
+    try {
+        const { product_id } = req.params;
+        const deleteproduct = await pool.query("DELETE FROM product WHERE product_id=$1", [product_id])
+
+        if (deleteproduct.rowCount > 0) {
+            res.status(200).send('Product deleted successfully');
+        } else {
+            res.status(404).send('Product not found');
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+
+};
+
+// edit product quantity
+const editProductQuantity= async (req, res) => {
+    const { quantity } = req.body;
+    const { product_id } = req.params;
+    if (quantity === undefined) {
+        return res.status(400).send("Quantity is required");
+    }
+    try {
+        const editquantity = await pool.query("UPDATE product SET quantity=$1 WHERE product_id=$2", [quantity, product_id])
+
+        if (editquantity.rowCount > 0) {
+            res.status(200).send('Product quantity updated successfully');
+        } else {
+            res.status(404).send('Product not found');
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+};
+
+// edit product price
+const editProductPrice= async (req, res) => {
+    const { price } = req.body;
+    const { product_id } = req.params;
+    if (price === undefined) {
+        return res.status(400).send("price is required");
+    }
+    try {
+        const editprice = await pool.query("UPDATE product SET price=$1 WHERE product_id=$2", [price, product_id])
+
+        if (editprice.rowCount > 0) {
+            res.status(200).send('Product price updated successfully');
+        } else {
+            res.status(404).send('Product not found');
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+};
+
+// get all products
+const getAllProducts= async (req, res) => {
+    try {
+        const selectQuery = 'SELECT * FROM product';
+        const result = await pool.query(selectQuery);
+        res.json(result.rows)
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+};
+
+
+
+
+
+
+
+module.exports={addingProduct,getProductDetails,deleteProduct,editProductQuantity,editProductPrice,getAllProducts}
