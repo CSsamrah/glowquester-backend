431812c024754362416312188f094f17647bf057
CSSamrah <samra.fatima.790@gmail.com>
2024-08-14 15:27:29 +0500

    added env variables

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
-const Pool=require('pg').Pool;
+const {Pool}=require('pg')
 
-const pool=new Pool({
-    user:"postgres",
-    password:"samrah",
-    host:"localhost",
-    port:5432,
-    database:"glowquester"
+const pool = new Pool({
+    user: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || "samrah",
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 5432,
+    database: process.env.DB_DATABASE || "glowquester"
 });
+
 module.exports=pool;
