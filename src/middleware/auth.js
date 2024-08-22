c9bc212e1b53b561ce7f5c430ab240a64953301f
CSSamrah <samra.fatima.790@gmail.com>
2024-08-22 20:19:25 +0500

    middleware changes

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,22 +1,27 @@
+const ipWhitelist = async (req, res, next) => {
+  try {
+      // Whitelisted IP addresses
+      const allowedIPs = ['202.47.38.217']; // Add the IPs you want to whitelist
 
-// Whitelisted IP addresses
-const allowedIPs = ['202.47.38.217']; // Add the IPs you want to whitelist
+      // Get the requester's IP address
+      const requesterIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
-// Middleware to check if the request's IP is whitelisted
-function ipWhitelist(req, res, next) {
-  // Get the requester's IP address
-  const requesterIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+      // Clean IP address (remove "::ffff:" prefix for IPv4 mapped addresses)
+      const cleanedIP = requesterIP.replace(/^::ffff:/, '');
 
-  // Clean IP address (remove "::ffff:" prefix for IPv4 mapped addresses)
-  const cleanedIP = requesterIP.replace(/^::ffff:/, '');
-
-  if (allowedIPs.includes(cleanedIP)) {
-    // If IP is whitelisted, proceed to the next middleware or route
-    next();
-  } else {
-    // If IP is not whitelisted, send a 403 Forbidden response
-    res.status(403).json({ message: 'Access forbidden: Your IP is not authorized to access this resource.' });
+      // Check if the IP is in the whitelist
+      if (allowedIPs.includes(cleanedIP)) {
+          // If IP is whitelisted, proceed to the next middleware or route
+          next();
+      } else {
+          // If IP is not whitelisted, send a 403 Forbidden response
+          res.status(403).json({ message: 'Access forbidden: Your IP is not authorized to access this resource.' });
+      }
+  } catch (error) {
+      // Log and respond with server error in case of failure
+      console.error(error);
+      res.status(500).send('Internal Server Error');
   }
-}
+};
 
-module.exports={ipWhitelist}
+module.exports = { ipWhitelist };
